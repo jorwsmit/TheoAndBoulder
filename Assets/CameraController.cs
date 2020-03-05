85042ca69dbf5d2a62aff84fb34a39b0ce156f0d
jorwsmit <jorwsmith92@gmail.com>
2020-03-04 23:22:29 -0500

    Removing some unnecessary scenes and adding some controllers.

---
+++ Assets/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+    public float smoothingAmmount = 0.125f;
+    public Vector3 cameraOffset;
+
+    private void LateUpdate()
+    {
+        //transform.position = target.position + cameraOffset;
+        transform.position = Vector3.Lerp(transform.position, target.position + cameraOffset, smoothingAmmount);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D player;
+    public float speed = 5f;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        player.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+    }
+}
